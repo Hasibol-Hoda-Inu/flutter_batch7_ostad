f0bd11dd7701333226a918553485a8410f07e05f
Enu <hasinulhuda@gmail.com>
2024-09-05 04:42:06 +0600

    the assignment has been completed

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -8,26 +8,38 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<HomeScreen> {
-  int _counter = 0;
-  int _total=0;
-  List <int> price=[51, 30, 43];
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-      _totalPriceCounter();
-    });
-  }
-  void _decrementCounter() {
-    setState(() {
-      _counter--;
-      _totalPriceCounter();
-    });
+  int _counterF = 0;
+  int _counterS = 0;
+  int _counterT = 0;
+
+  int _totalF=0;
+  int _totalS=0;
+  int _totalT=0;
+
+  String _totalAmountCalculation(){
+    int _totalAmount= _totalF+_totalS+_totalT;
+    return _totalAmount.toString();
   }
 
-  void _totalPriceCounter(){
-    _total= _counter*price[0];
-    setState(() {});
-  }
+  List <int> price=[51, 30, 43];
+
+  // void _totalPriceCounter(){
+  //   _total= _counter*price[0];
+  //   setState(() {});
+  // }
+  //
+  // void _incrementCounter() {
+  //   setState(() {
+  //     _counter++;
+  //     _totalPriceCounter();
+  //   });
+  // }
+  // void _decrementCounter() {
+  //   setState(() {
+  //     _counter--;
+  //     _totalPriceCounter();
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -40,7 +52,7 @@ class _MyHomePageState extends State<HomeScreen> {
         ),),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(16),
         child: Column(
           children: [
             Expanded(
@@ -50,7 +62,7 @@ class _MyHomePageState extends State<HomeScreen> {
                     color: Colors.white,
                     height: 140,
                     width: double.infinity,
-                    padding: const EdgeInsets.all(16),
+                    padding: const EdgeInsets.only(left: 0, top: 16, right: 16, bottom: 16),
                     child: Row(
                       children: [
                         Image.asset('assets/images/shirt.jpg', width:140,),
@@ -59,7 +71,7 @@ class _MyHomePageState extends State<HomeScreen> {
                               crossAxisAlignment: CrossAxisAlignment.start,
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: [
-                                Column(
+                                const Column(
                                    crossAxisAlignment: CrossAxisAlignment.start,
                                    children: [
                                    Text('Pullover', style: TextStyle(
@@ -93,12 +105,14 @@ class _MyHomePageState extends State<HomeScreen> {
                                       child: IconButton(
                                         icon: const Icon(Icons.remove),
                                         onPressed: () {
-                                          _decrementCounter();
+                                          _counterF--;
+                                          _totalF= _counterF*price[0];
+                                          setState(() {});
                                         },
                                       ),
                                     ),
                                     const SizedBox(width: 16,),
-                                    Text("$_counter", style: const TextStyle(
+                                    Text("$_counterF", style: const TextStyle(
                                       fontSize: 20,
                                       fontWeight: FontWeight.w600
                                     ),),
@@ -110,7 +124,9 @@ class _MyHomePageState extends State<HomeScreen> {
                                       child: IconButton(
                                         icon: const Icon(Icons.add),
                                         onPressed: () {
-                                          _incrementCounter();
+                                          _counterF++;
+                                          _totalF= _counterF*price[0];
+                                          setState(() {});
                                         },
                                       ),
                                     ),
@@ -132,6 +148,188 @@ class _MyHomePageState extends State<HomeScreen> {
                       ],
                     ),
                   ),
+                  const SizedBox(height: 16,),
+                  Container(
+                    color: Colors.white,
+                    height: 140,
+                    width: double.infinity,
+                    padding: const EdgeInsets.only(left: 0, top: 16, right: 16, bottom: 16),
+                    child: Row(
+                      children: [
+                        Image.asset('assets/images/shirt.jpg', width:140,),
+                        Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                const Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text('Pullover', style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w600,
+                                    ),),
+                                    Row(
+                                      children: [
+                                        Text('Color: ', style: TextStyle(
+                                          fontSize: 16,
+                                        ),),
+                                        Text('Black', style: TextStyle(
+                                          fontSize: 16,
+                                        ),),
+                                        SizedBox(width: 16,),
+                                        Text('Size: ', style: TextStyle(
+                                          fontSize: 16,
+                                        ),),
+                                        Text('L', style: TextStyle(
+                                          fontSize: 16,
+                                        ),),
+                                      ],
+                                    ),
+                                  ],),
+                                Row(
+                                  children: [
+                                    Material(
+                                      elevation: 2.0,
+                                      shape: const CircleBorder(),
+                                      color: Colors.white,
+                                      child: IconButton(
+                                        icon: const Icon(Icons.remove),
+                                        onPressed: () {
+                                          _counterS--;
+                                          _totalS= _counterS*price[1];
+                                          setState(() {});
+                                        },
+                                      ),
+                                    ),
+                                    const SizedBox(width: 16,),
+                                    Text("$_counterS", style: const TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w600
+                                    ),),
+                                    const SizedBox(width: 16,),
+                                    Material(
+                                      elevation: 2.0,
+                                      shape: const CircleBorder(),
+                                      color: Colors.white,
+                                      child: IconButton(
+                                        icon: const Icon(Icons.add),
+                                        onPressed: () {
+                                          _counterS++;
+                                          _totalS= _counterS*price[1];
+                                          setState(() {});
+                                        },
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            )),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            const Icon(Icons.more_vert_rounded, color: Colors.grey,),
+                            Text("${price[1]}\$", style: const TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w700,
+                            ),)
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 16,),
+                  Container(
+                    color: Colors.white,
+                    height: 140,
+                    width: double.infinity,
+                    padding: const EdgeInsets.only(left: 0, top: 16, right: 16, bottom: 16),
+                    child: Row(
+                      children: [
+                        Image.asset('assets/images/shirt.jpg', width:140,),
+                        Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                const Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text('Pullover', style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w600,
+                                    ),),
+                                    Row(
+                                      children: [
+                                        Text('Color: ', style: TextStyle(
+                                          fontSize: 16,
+                                        ),),
+                                        Text('Black', style: TextStyle(
+                                          fontSize: 16,
+                                        ),),
+                                        SizedBox(width: 16,),
+                                        Text('Size: ', style: TextStyle(
+                                          fontSize: 16,
+                                        ),),
+                                        Text('L', style: TextStyle(
+                                          fontSize: 16,
+                                        ),),
+                                      ],
+                                    ),
+                                  ],),
+                                Row(
+                                  children: [
+                                    Material(
+                                      elevation: 2.0,
+                                      shape: const CircleBorder(),
+                                      color: Colors.white,
+                                      child: IconButton(
+                                        icon: const Icon(Icons.remove),
+                                        onPressed: () {
+                                          _counterT--;
+                                          _totalT= _counterT*price[2];
+                                          setState(() {});
+                                        },
+                                      ),
+                                    ),
+                                    const SizedBox(width: 16,),
+                                    Text("$_counterT", style: const TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w600
+                                    ),),
+                                    const SizedBox(width: 16,),
+                                    Material(
+                                      elevation: 2.0,
+                                      shape: const CircleBorder(),
+                                      color: Colors.white,
+                                      child: IconButton(
+                                        icon: const Icon(Icons.add),
+                                        onPressed: () {
+                                          _counterT++;
+                                          _totalT= _counterT*price[2];
+                                          setState(() {});
+                                        },
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            )),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            const Icon(Icons.more_vert_rounded, color: Colors.grey,),
+                            Text("${price[2]}\$", style: const TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w700,
+                            ),)
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ),
@@ -142,7 +340,7 @@ class _MyHomePageState extends State<HomeScreen> {
                   const Text('Total amount',style: TextStyle(
                     fontSize: 18,
                   ),),
-                  Text('$_total\$', style: const TextStyle(
+                  Text("${_totalAmountCalculation()}\$", style: const TextStyle(
                     fontSize: 22,
                     fontWeight: FontWeight.bold,
                   ),),
@@ -153,7 +351,16 @@ class _MyHomePageState extends State<HomeScreen> {
                 width: double.infinity,
                 height: 60,
                 child: ElevatedButton(
-                  onPressed: (){},
+                  onPressed: (){
+                    ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text('Congratulations', style: TextStyle(fontSize: 18),),
+                          behavior: SnackBarBehavior.floating,
+                          margin: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+                          backgroundColor: Colors.green,
+                        )
+                    );
+                  },
                   style: ElevatedButton.styleFrom(
                       backgroundColor: const Color(0xffDB3022)
                   ),
