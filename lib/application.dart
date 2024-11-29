da572483110857377a055dee4a9249757eff8e68
Enu <hasinulhuda@gmail.com>
2024-11-29 16:28:34 +0600

    added google map to the project

--- lib/application.dart
+++ lib/application.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:google_map_practice/presentation/home_screen.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
   }
 }
--- lib/presentation/home_screen.dart
+++ lib/presentation/home_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,6 +13,24 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: LatLng(25.282623959000336, 89.01496503289209,),
+          zoom: 16,
+
+      ),
+        onTap: (LatLng? latLang){
+          print(latLang);
+        },
+        mapType: MapType.normal,
+        zoomGesturesEnabled: true,
+        tiltGesturesEnabled: true,
+        myLocationEnabled: true,
+        myLocationButtonEnabled: true,
+        trafficEnabled: true,
+
+      ),
+    );
   }
 }
